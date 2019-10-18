838895aaaf68fa52e91caf9dafb8ccc6aa5d0004
jbeaulie <beaulieu.jake@epa.gov>
2019-10-18 11:21:40 -0400

    migrated points from issues.R to github Issues feature

--- scripts/issues.R
+++ scripts/issues.R
@@ -12,7 +12,5 @@
 # e-mailed Joel about samples collected after 4/4/2018
 
 
-## project name
-# repo is 'reservoir_methane', which isn't very informative given all
-# the reservoir-CH4 work I do.  Lets change to something, maybe
-# ENSB, which is a name John used in O-drive.  What does this stand for?
+## TOC data
+
